5603916d08163fbdeb7696ef3099580d07a32233
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2018-02-20 15:04:32 -0500

    -	You need to use distinct integers for each Enum names. -	One of the self. _flipstate was misspelled as self._flip -	You didn’t need to have print nder Get_x: -	You needed to initialize the seed with the id of each game so that your games are distinct from each other.

--- Problem 1.py
+++ Problem 1.py
@@ -3,10 +3,10 @@ from enum import Enum
 import numpy as np
 
 class FlipState(Enum):
-    HEADS=0
-    TAILS=0
-    TWOTAILS=0
-    WIN=1
+    HEADS=1
+    TAILS=2
+    TWOTAILS=3
+    WIN=4
 # This is actually a Markov process model
 # because you're either in the state where you just got heads and you need
 # two tails then a heads to win or you're in the state where you just got
@@ -15,8 +15,9 @@ class FlipState(Enum):
 #and by extension start over.
 
 class Game:
-    def __init__(self, odds_heads):
+    def __init__(self, id, odds_heads):
         self._rnd=np.random
+        self._rnd.seed(id)
         self.odds_heads=odds_heads
         self._wins=0
         self._flipstate=FlipState.HEADS
@@ -28,7 +29,7 @@ class Game:
             #You either get heads and stay in the "heads" state or you get tails and then
             #move into the place where you need one more tails followed by a heads to win.
             if self._flipstate==FlipState.HEADS:
-                if np.random.sample() < self.odds_heads:
+                if self._rnd.sample() < self.odds_heads:
                     self._flipstate=FlipState.HEADS
                 else:
                     self._flipstate=FlipState.TAILS
@@ -36,15 +37,15 @@ class Game:
             #one more followed by a heads to win. If you flip heads on this one, you
             #move back to the previous "heads" state.
             elif self._flipstate==FlipState.TAILS:
-                if np.random.sample() < self.odds_heads:
+                if self._rnd.sample() < self.odds_heads:
                     self._flipstate=FlipState.HEADS
                 else:
-                    self._flip=FlipState.TWOTAILS
+                    self._flipstate=FlipState.TWOTAILS
             #This is the two tails flipstate. In this state you either flip heads (and win)
             #Or you flip tails and you've already got 2 tails built up so you stay in the
             #two tails state.
             elif self._flipstate==FlipState.TWOTAILS:
-                if np.random.sample() < self.odds_heads:
+                if self._rnd.sample() < self.odds_heads:
                     self._flipstate=FlipState.WIN
                     self._wins+=1
                 else:
@@ -54,17 +55,17 @@ class Game:
             # which is the same as the Heads flipstate so we're going to give it the same syntax
             #and push it back into the loop.
             elif self._flipstate==FlipState.WIN:
-                if np.random.sample() < self.odds_heads:
+                if self._rnd.sample() < self.odds_heads:
                     self._flipstate=FlipState.HEADS
                 else:
                     self._flipstate=FlipState.TAILS
             else:
                 print (["Error at", t])
-            t+1
+            t += 1
 
         ##we need to count the times that it gets tails, tails, heads in a 20 flip sequence
     def Get_x(self):
-        print((self._wins*100)-250)
+        return self._wins*100-250
 
 class AllGames:
     def __init__(self, odds_heads, num_games):
@@ -72,18 +73,17 @@ class AllGames:
         self._num_games=num_games
         self._outcomes=[]
 
-    def simulate(self, num_games):
-        for game in len(self._num_games):
-            game.simulate(num_games)
+    def simulate(self):
+        for i in range(self._num_games):
+            game = Game(i, self.odds_heads)
+            game.one_game()
             self._outcomes.append(game.Get_x())
 
     def get_average_x(self):
         return sum(self._outcomes)/self._num_games
-        print(self._outcomes)/self._num_games
 
-MyGame = Game(odds_heads=.5)
-MyGame.Get_x()
 
 MySimulation=AllGames(odds_heads=.5, num_games=1000)
-MySimulation.get_average_x()
+MySimulation.simulate()
+print(MySimulation.get_average_x())
 
